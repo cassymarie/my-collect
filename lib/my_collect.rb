59074cc5e1cc0640d07c34a570b0142d734b3c0a
cassymarie <cassy2205@gmail.com>
2020-10-21 12:04:29 -0600

    commit tree 500b49e6cb367ffd27dbf3fd4f7918528a6c69d1

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+        counter = 0
+        new_collection = []
+        while counter < array.length do
+            new_collection << yield(array[counter])
+            counter += 1
+        end
+        return new_collection
+end
 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
+my_collect(languages) { |name| "#{name.upcase}" }
